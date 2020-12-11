b0fe2101d68b0308b66125d8a52c08475176a0e4
9631kunn <infourbh+1@gmail.com>
2020-12-11 12:04:56 +0900

    add: form

--- next-firebase/pages/users/[uid].tsx
+++ next-firebase/pages/users/[uid].tsx
@@ -34,6 +34,18 @@ export default function UserShow() {
         <div className="text-center">
           <h1 className="h4">{user.name}さんのページ</h1>
           <p className="h5">{user.name}さんへの質問</p>
+          <div className="row justify-content-center mb-3">
+            <div className="col-12 col-md-6">
+              <form>
+                <textarea name="question" className="form-control" placeholder="TEST" rows={6} required />
+                <div className="m-3">
+                  <button type="submit" className="btn btn-primary">
+                    質問を送信する
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       )}
     </Layout>
